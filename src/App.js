2fd32b469e07945500a0c146d2508db72314c85e
Lucas Rosa Rodrigues <lcs-rodrigues@hotmail.com>
2021-08-04 10:32:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+const axios = require('axios');
+
+function App() {
+
+  const [lstNomes, setLstNomes] = useState([]);
+
+  useEffect(() => {
+    function getNomes() {
+
+      axios.get('http://localhost:3030/listar')
+        .then(function (response) {
+          setLstNomes(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+
+    getNomes();
+
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <ul>
+          {
+            lstNomes.length ?
+              lstNomes.map(data => {
+                return (
+                  <li>{data.Nome}</li>
+                )
+              })
+              :
+              <li>A lista está vazia</li>
+          }
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cadastrar.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { useState } from 'react';
+const axios = require('axios');
+
+function App() {
+
+    const [nome, setNome] = useState("");
+
+    function saveName() {
+
+        const data = { "Nome": nome };
+
+        axios.post('http://localhost:3030/cadastrar', data)
+            .then(function (response) {
+                console.log(response);
+                alert('Cadastrado com sucesso');
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    function eventHandler(e) {
+        setNome(e.target.value);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <label>Digite seu nome:</label>
+                <input type="text" placeholder="Digite seu nome" onBlur={(e) => eventHandler(e)} />
+                <input type="button" value="Salvar" onClick={saveName} />
+            </header>
+        </div>
+    );
+}
+
+export default App;
